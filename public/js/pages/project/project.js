20a6c465a59037035bbb8f2db119526346a2715c
Administrator <1508840940@qq.com>
2020-03-23 09:48:25 +0800

    111

--- public/js/pages/project/project.js
+++ public/js/pages/project/project.js
@@ -725,53 +725,79 @@ function changeButtonStatus(){
 var uploader = WebUploader.create({
     // swf文件路径
     swf: '/public/js/plugins/webuploader/Uploader.swf',
-
     // 文件接收服务端。
-    server: 'http://webuploader.duapp.com/server/fileupload.php',
-
+    // server: 'http://webuploader.duapp.com/server/fileupload.php',
+    server: 'http://192.168.10.14:8001/zsdev/ac/web/front/fileUpload',
     // 选择文件的按钮。可选。
     // 内部根据当前运行是创建，可能是input元素，也可能是flash.
     pick: '#picker',
-
     // 不压缩image, 默认如果是jpeg，文件上传前会压缩一把再上传！
     resize: false
 })
 
-// 当有文件被添加进队列的时候
-uploader.on( 'fileQueued', function( file ) {
-    $list.append( '<div id="' + file.id + '" class="item">' +
-        '<h4 class="info">' + file.name + '</h4>' +
-        '<p class="state">等待上传...</p>' +
-        '</div>' );
-});
-// 文件上传过程中创建进度条实时显示。
-uploader.on( 'uploadProgress', function( file, percentage ) {
-    var $li = $( '#'+file.id ),
-        $percent = $li.find('.progress .progress-bar');
+    // 当有文件被添加进队列的时候
+    uploader.on( 'fileQueued', function( file ) {
+        $("#thelist").append( '<div id="' + file.id + '" class="item">' +
+            '<h4 class="info">' + file.name + '</h4>' +
+            '<p class="state">等待上传...</p>' +
+            '</div>' );
+    });
+    // 文件上传过程中创建进度条实时显示。
+    uploader.on( 'uploadProgress', function( file, percentage ) {
+        var $li = $( '#'+file.id ),
+            $percent = $li.find('.progress .progress-bar');
 
-    // 避免重复创建
-    if ( !$percent.length ) {
-        $percent = $('<div class="progress progress-striped active">' +
-            '<div class="progress-bar" role="progressbar" style="width: 0%">' +
-            '</div>' +
-            '</div>').appendTo( $li ).find('.progress-bar');
-    }
+        // 避免重复创建
+        if ( !$percent.length ) {
+            $percent = $('<div class="progress progress-striped active">' +
+                '<div class="progress-bar" role="progressbar" style="width: 0%">' +
+                '</div>' +
+                '</div>').appendTo( $li ).find('.progress-bar');
+        }
 
-    $li.find('p.state').text('上传中');
+        $li.find('p.state').text('上传中');
 
-    $percent.css( 'width', percentage * 100 + '%' );
-});
-uploader.on( 'uploadSuccess', function( file ) {
-    $( '#'+file.id ).find('p.state').text('已上传');
-});
+        $percent.css( 'width', percentage * 100 + '%' );
+    });
+    uploader.on( 'uploadSuccess', function( file ) {
+        $( '#'+file.id ).find('p.state').text('已上传');
+    });
 
-uploader.on( 'uploadError', function( file ) {
-    $( '#'+file.id ).find('p.state').text('上传出错');
-});
+    uploader.on( 'uploadError', function( file ) {
+        $( '#'+file.id ).find('p.state').text('上传出错');
+    });
 
-uploader.on( 'uploadComplete', function( file ) {
-    $( '#'+file.id ).find('.progress').fadeOut();
-});
+    uploader.on( 'uploadComplete', function( file ) {
+        $( '#'+file.id ).find('.progress').fadeOut();
+    });
+
+
+    //开始上传
+    $('#ctlBtn').bind('click', function (e) {
+        console.log(e)
+        console.log("点击上传")
+        uploader.upload();
+    });
+    // 当开始上传流程时触发
+    uploader.on('startUpload', function () {
+        console.log("开始上传startUpload");
+        $("#uploadPlaceholder").hide();
+    });
+    // 当所有文件上传结束时触发
+    uploader.on('uploadFinished', function () {
+        console.log("上传结束uploadFinished");
+        $("#uploadPlaceholder").show();
+    });
+
+    uploader.on('error', function (type, arg, file) {
+        if (type == "Q_TYPE_DENIED") {
+            //格式问题
+        } else if (type == "Q_EXCEED_SIZE_LIMIT") {
+            //大小限制
+        } else {
+            //其他错误
+        }
+    });
 
 
 
